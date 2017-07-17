689809044e1873c9de491f809807fb9b541d7b11
Lix <xiao.liunit@gmail.com>
2017-07-17 12:13:49 +0800

    First commit

---
+++ src/FirstSample.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Lix on 2017/7/17.
+ */
+public class FirstSample {
+    public static void main(String[] args)
+    {
+        System.out.println("We will not use 'Hello, world'");
+    }
+}
