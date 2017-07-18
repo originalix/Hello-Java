269d6c8d4231f19d78ce3f172b43269d64d0c98f
Lix <xiao.liunit@gmail.com>
2017-07-18 12:38:42 +0800

    constant class

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -7,3 +7,15 @@ public class FirstSample {
         System.out.println("We will not use 'Hello, world'");
     }
 }
+
+class Constants
+{
+    public static final double CM_PER_INCH = 2.54;
+    public static void main(String[] args)
+    {
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: "
+        + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+    }
+}
