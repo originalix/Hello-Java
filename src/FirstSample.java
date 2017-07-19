42fe23ed1dd0924492f052f660c95821e94529c2
Lix <xiao.liunit@gmail.com>
2017-07-19 12:07:41 +0800

    system in

--- src/FirstSample.java
+++ src/FirstSample.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Lix on 2017/7/17.
  */
@@ -11,11 +13,16 @@ public class FirstSample {
 class Constants
 {
     public static final double CM_PER_INCH = 2.54;
-    public static void main(String[] args)
+    public static strictfp void main(String[] args)
     {
         double paperWidth = 8.5;
         double paperHeight = 11;
         System.out.println("Paper size in centimeters: "
         + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("What's u name");
+        String name = in.nextLine();
+        System.out.println("Hello " + name);
     }
 }
