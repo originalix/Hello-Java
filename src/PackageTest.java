f50fcf9c723ac5efd309e7438e45a20805e8c2e4
Lix <xiao.liunit@gmail.com>
2017-07-20 10:37:32 +0800

    test package

--- src/PackageTest.java
+++ src/PackageTest.java
@@ -1,5 +1,13 @@
 /**
  * Created by Lix on 2017/7/20.
  */
+
+import corejava.Employee;
+
 public class PackageTest {
+    public static void main(String[] args) {
+        Employee wsx = new Employee("Wsx", 50000, 1992, 12, 1);
+        wsx.raiseSalary(5);
+        System.out.println("name = " + wsx.getName() + ". Salary = " + wsx.getSalary());
+    }
 }
