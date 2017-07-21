4870db1f846f1ed0e3c8306e75c856ca1d4eccd0
Lix <xiao.liunit@gmail.com>
2017-07-22 07:18:11 +0800

    finish student class

--- src/corejava/Student.java
+++ src/corejava/Student.java
@@ -3,5 +3,15 @@ package corejava;
 /**
  * Created by Lix on 2017/7/22.
  */
-public class Student {
+public class Student extends Person {
+    private String major;
+
+    public Student(String n, String m) {
+        super(n);
+        major = m;
+    }
+
+    public String getDescription() {
+        return "A student majoring in " + major;
+    }
 }
