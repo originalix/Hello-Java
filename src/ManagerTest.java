4484333e78dbb479da36b241d0d74ab9e30a3c2b
Lix <xiao.liunit@gmail.com>
2017-07-21 14:43:16 +0800

    finish ManagerTest

--- src/ManagerTest.java
+++ src/ManagerTest.java
@@ -1,5 +1,20 @@
 /**
  * Created by Lix on 2017/7/21.
  */
+import corejava.*;
+
 public class ManagerTest {
+    public static void main(String[] args) {
+        Manager boss = new Manager("Lix", 120000, 1993, 1, 27);
+        boss.setBonus(8888);
+
+        Employee[] staff = new Employee[3];
+        staff[0] = boss;
+        staff[1] = new Employee("Wsx", 80000, 1992, 12, 01);
+        staff[2] = new Employee("Leon", 99999, 1993, 07, 24);
+
+        for(Employee e : staff) {
+            System.out.println("name : " + e.getName() + " , Salary: " + e.getSalary());
+        }
+    }
 }
