2aa4b2f369ca421c342fa7096eac9230d748a186
Lix <xiao.liunit@gmail.com>
2017-07-21 14:13:18 +0800

    initialize Manager Class

--- src/corejava/Manager.java
+++ src/corejava/Manager.java
@@ -3,5 +3,20 @@ package corejava;
 /**
  * Created by Lix on 2017/7/21.
  */
-public class Manager {
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String n, double s, int year, int month, int day) {
+        super(n, s, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b) {
+        bonus = b;
+    }
 }
