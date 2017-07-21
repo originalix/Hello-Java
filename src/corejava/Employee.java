776141c4c544c5bdd8fde270ead0ab6a66e2cbb1
Lix <xiao.liunit@gmail.com>
2017-07-22 07:21:09 +0800

    made Employee class extends Person class

--- src/corejava/Employee.java
+++ src/corejava/Employee.java
@@ -6,13 +6,13 @@ import java.util.GregorianCalendar;
 /**
  * Created by Lix on 2017/7/20.
  */
-public class Employee {
+public class Employee extends Person {
     private String name;
     private double salary;
     private Date hireDay;
 
     public Employee(String n, double s, int year, int month, int day) {
-        name = n;
+        super(n);
         salary = s;
         GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
         hireDay = calendar.getTime();
@@ -22,8 +22,13 @@ public class Employee {
         return name;
     }
 
-    public double getSalary() {
+    public double getSalary()
+    {
         return salary;
+    }
+
+    public String getDescription() {
+        return String.format("An employee with a salary of $%.2f", salary);
     }
 
     public Date getHireDay() {
