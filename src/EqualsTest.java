498e9d5297a902d0227adca5b9e934d93eacae70
Lix <xiao.liunit@gmail.com>
2017-07-24 22:00:36 +0800

    hash code test

--- src/EqualsTest.java
+++ src/EqualsTest.java
@@ -16,5 +16,15 @@ public class EqualsTest {
         System.out.println("wsx1.equals(wsx3): " + wsx1.equals(wsx3));
         System.out.println("wsx1.equals(lix): " + wsx1.equals(lix));
         System.out.println("lix.toString(): " + lix);
+
+        Manager carl = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+        boss.setBonus(5000);
+        System.out.println("boss toString(): " + boss);
+        System.out.println("carl.equals(boss): " + carl.equals(boss));
+        System.out.println("wsx1.hashCode(): " + wsx1.hashCode());
+        System.out.println("wsx3.hashCode()" + wsx3.hashCode());
+        System.out.println("lix.hashCode(): " + lix.hashCode());
+        System.out.println("carl.hashCode(): " + carl.hashCode());
     }
 }
