b9165f0b31973183fc4b066799fa2936a9042461
Lix <xiao.liunit@gmail.com>
2017-07-25 08:14:45 +0800

    finish Size enum

--- src/EnumTest.java
+++ src/EnumTest.java
@@ -3,3 +3,20 @@
  */
 public class EnumTest {
 }
+
+enum Size {
+    SMALL("S"),
+    MEDIUM("M"),
+    LARGE("L"),
+    EXTRA_LARGE("XL");
+
+    private String abbreviation;
+
+    private Size(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+}
