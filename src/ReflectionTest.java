4b9a86b1c1dfb7d56adee6d5d7beac2e070aa3c6
Lix <xiao.liunit@gmail.com>
2017-07-25 11:48:45 +0800

    printFields function

--- src/ReflectionTest.java
+++ src/ReflectionTest.java
@@ -1,5 +1,21 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 /**
  * Created by Lix on 2017/7/25.
  */
 public class ReflectionTest {
+    public static void printFields(Class cl) {
+        Field[] fields = cl.getDeclaredFields();
+
+        for (Field f : fields) {
+            Class type = f.getType();
+            String name = f.getName();
+            System.out.println("  ");
+            String modifiers = Modifier.toString(f.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+        }
+    }
 }
