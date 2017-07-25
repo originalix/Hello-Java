a81dfa55c04605fbad950bc197d68532caa8edc9
Lix <xiao.liunit@gmail.com>
2017-07-25 12:02:10 +0800

    print Method function

--- src/ReflectionTest.java
+++ src/ReflectionTest.java
@@ -1,10 +1,37 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 /**
  * Created by Lix on 2017/7/25.
  */
 public class ReflectionTest {
+
+    public static void printMethods(Class cl) {
+        Method[] methods = cl.getDeclaredMethods();
+        for (Method m : methods) {
+            Class retType = m.getReturnType();
+            String name = m.getName();
+
+            System.out.print("  ");
+
+            String modifiers = Modifier.toString(m.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(retType.getName() + " " + name + "(");
+
+            Class[] paramTypes = m.getParameterTypes();
+            for (int j = 0; j < paramTypes.length; j++) {
+                if (j > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[j].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
     /**
      * Prints all fields of a class
      * @param cl a class
