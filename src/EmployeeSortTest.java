5b7206d866b5165bf02e051e7a041984a59d5771
Lix <xiao.liunit@gmail.com>
2017-07-25 15:15:22 +0800

    finish compare function test

--- src/EmployeeSortTest.java
+++ src/EmployeeSortTest.java
@@ -1,5 +1,22 @@
+import corejava.Employee;
+
+import java.util.Arrays;
+
 /**
  * Created by Lix on 2017/7/25.
  */
 public class EmployeeSortTest {
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Harry Hacker", 95000, 1993, 1, 27);
+        staff[1] = new Employee("Carl Cracker", 75000, 1992, 12, 1);
+        staff[2] = new Employee("Tony Tester", 38000, 1991, 7, 24);
+
+        Arrays.sort(staff);
+
+        for (Employee e : staff) {
+            System.out.println("name = " + e.getName() + ", salary = " + e.getSalary());
+        }
+    }
 }
