7fe80af9704be7d1ffb78586874b76e1b6373fe0
Lix <xiao.liunit@gmail.com>
2017-07-25 12:28:04 +0800

    reflection test function

--- src/ReflectionTest.java
+++ src/ReflectionTest.java
@@ -2,11 +2,47 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Objects;
+import java.util.Scanner;
 
 /**
  * Created by Lix on 2017/7/25.
  */
 public class ReflectionTest {
+
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter class name(e.g. java.util.Date): ");
+            name = in.next();
+        }
+
+        try {
+            Class cl = Class.forName(name);
+            Class supercl = cl.getSuperclass();
+            String modifiers = Modifier.toString(cl.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print("class " + name);
+            if (supercl != null && supercl != Objects.class) {
+                System.out.print("extends " + supercl.getName());
+            }
+            System.out.print("\n{\n");
+            printConstructors(cl);
+            System.out.println();
+            printMethods(cl);
+            System.out.println();
+            printFields(cl);
+            System.out.println("}");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
 
     /**
      * Print all constructors of a class
