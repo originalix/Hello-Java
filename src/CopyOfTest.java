b903ca6a9d5a6e9c69babe72383ee5fa874cfb14
Lix <xiao.liunit@gmail.com>
2017-07-25 13:45:04 +0800

    badCopy function

--- src/CopyOfTest.java
+++ src/CopyOfTest.java
@@ -1,5 +1,14 @@
+import static java.lang.System.*;
+
 /**
  * Created by Lix on 2017/7/25.
  */
 public class CopyOfTest {
+    public static Object[] badCopyOf(Object[] a, int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(a, 0, newArray, 0, Math.min(a.length, newLength));
+        return newArray;
+    }
+
+    
 }
