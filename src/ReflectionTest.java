fc65456b84e52b0cbba7f487e1e3eb517596c0ba
Lix <xiao.liunit@gmail.com>
2017-07-25 12:14:10 +0800

    print all constructors of a class

--- src/ReflectionTest.java
+++ src/ReflectionTest.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -6,6 +7,32 @@ import java.lang.reflect.Modifier;
  * Created by Lix on 2017/7/25.
  */
 public class ReflectionTest {
+
+    /**
+     * Print all constructors of a class
+     * @param cl a class
+     */
+    public static void printConstructors(Class cl) {
+        Constructor[] constructors = cl.getDeclaredConstructors();
+        for (Constructor c : constructors) {
+            String name = c.getName();
+            System.out.print("  ");
+            String modifiers = Modifier.toString(c.getModifiers());
+            if (modifiers.length() > 0) {
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(name + "(");
+
+            Class[] paramTypes = c.getParameterTypes();
+            for (int j = 0; j < paramTypes.length; j++) {
+                if (j > 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(paramTypes[j].getName());
+            }
+            System.out.println(");");
+        }
+    }
 
     /**
      * Prints all methods of a class
