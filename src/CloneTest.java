f12d8cb46cbc09c4bba57a7585e9acc241263b67
Lix <xiao.liunit@gmail.com>
2017-07-27 11:38:46 +0800

    main for clone test

--- src/CloneTest.java
+++ src/CloneTest.java
@@ -1,5 +1,15 @@
+import corejava.Employee;
+
 /**
  * Created by Lix on 2017/7/27.
  */
 public class CloneTest {
+    public static void main(String[] args) {
+        try {
+            Employee original = new Employee("Jhon Q. Public", 5000, 1993, 1, 27);
+            Employee copy = original.clone();
+            copy.raiseSalary(10);
+//            copy.set
+        }
+    }
 }
