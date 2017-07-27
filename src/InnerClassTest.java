fae972adf4a06c2c060487323ea31a351dc8833c
Lix <xiao.liunit@gmail.com>
2017-07-27 13:58:49 +0800

    update innerClassTest

---
+++ src/InnerClassTest.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by Lix on 2017/7/27.
+ */
+public class InnerClassTest {
+    public static void main(String[] args) {
+        TalkingClock clock = new TalkingClock(1000, true);
+        clock.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
