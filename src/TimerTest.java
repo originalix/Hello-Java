edecc9d37d82d8ee6e46b7f283eb6ecd2158ea7b
Lix <xiao.liunit@gmail.com>
2017-07-27 12:16:17 +0800

    Action Listener

--- src/TimerTest.java
+++ src/TimerTest.java
@@ -1,5 +1,18 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
 /**
  * Created by Lix on 2017/7/27.
  */
 public class TimerTest {
+}
+
+class TimePrinter implements ActionListener {
+    public void actionPerformed(ActionEvent event) {
+        Date now = new Date();
+        System.out.println("At the tone, the time is" + now);
+        Toolkit.getDefaultToolkit().beep();
+    }
 }
