801a4eb66f008c0da8ba47e6bdc6011b9cb2e35e
Lix <xiao.liunit@gmail.com>
2017-07-27 12:20:18 +0800

    test timer listener

--- src/TimerTest.java
+++ src/TimerTest.java
@@ -1,12 +1,22 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
+import javax.swing.Timer;
 
 /**
  * Created by Lix on 2017/7/27.
  */
 public class TimerTest {
+    public static void main(String[] args) {
+        ActionListener listener = new TimePrinter();
+
+        Timer t = new Timer(10000, listener);
+        t.start();
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
 }
 
 class TimePrinter implements ActionListener {
