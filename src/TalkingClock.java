48f6ee666438d669d28917289f9083c4ea6c9bc8
Lix <xiao.liunit@gmail.com>
2017-07-27 13:50:25 +0800

    finish TalkingClock Class

--- src/TalkingClock.java
+++ src/TalkingClock.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +10,25 @@ import java.util.Date;
 public class TalkingClock {
     private int interval;
     private boolean beep;
+
+    /**
+     * Constructs a talking clock.
+     * @param interval the interval between messages(in milliseconds)
+     * @param beep true if the clock should beep
+     */
+    public TalkingClock(int interval, boolean beep) {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    /**
+     * Strats the clock.
+     */
+    public void start() {
+        ActionListener listener = new TimePrinter();
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
 
     public class TimePrinter implements ActionListener {
         public void actionPerformed(ActionEvent event) {
