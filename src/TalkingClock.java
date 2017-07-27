3d1284cafedb8eaad3c7fad5ab8d051f9300181d
Lix <xiao.liunit@gmail.com>
2017-07-27 12:38:04 +0800

    create TimePrinter Class in TalkingClock

--- src/TalkingClock.java
+++ src/TalkingClock.java
@@ -1,5 +1,23 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
 /**
  * Created by Lix on 2017/7/27.
  */
 public class TalkingClock {
+    private int interval;
+    private boolean beep;
+
+    public class TimePrinter implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Date now = new Date();
+            System.out.println("At the tone, the time is  " + now);
+            if (beep) {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
 }
+
