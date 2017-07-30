5d809d5b53af12a261a5db15320445d1ba0e64ff
Lix <xiao.liunit@gmail.com>
2017-07-30 15:31:23 +0800

    create Pair static class

--- src/ArrayAlg.java
+++ src/ArrayAlg.java
@@ -2,4 +2,21 @@
  * Created by Lix on 2017/7/30.
  */
 public class ArrayAlg {
+    public static class Pair {
+        private double first;
+        private double second;
+
+        public Pair(double f, double s) {
+            first = f;
+            second = s;
+        }
+
+        public double getFirst() {
+            return first;
+        }
+
+        public double getSecond() {
+            return second;
+        }
+    }
 }
