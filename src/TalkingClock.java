6095a737f698e6e361c6034f200fc456e7f66da4
Lix <xiao.liunit@gmail.com>
2017-07-30 15:09:18 +0800

    reset TalkingClock class

--- src/TalkingClock.java
+++ src/TalkingClock.java
@@ -8,36 +8,6 @@ import java.util.Date;
  * Created by Lix on 2017/7/27.
  */
 public class TalkingClock {
-    private int interval;
-    private boolean beep;
 
-    /**
-     * Constructs a talking clock.
-     * @param interval the interval between messages(in milliseconds)
-     * @param beep true if the clock should beep
-     */
-    public TalkingClock(int interval, boolean beep) {
-        this.interval = interval;
-        this.beep = beep;
-    }
-
-    /**
-     * Strats the clock.
-     */
-    public void start() {
-        ActionListener listener = new TimePrinter();
-        Timer t = new Timer(interval, listener);
-        t.start();
-    }
-
-    public class TimePrinter implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            Date now = new Date();
-            System.out.println("At the tone, the time is  " + now);
-            if (beep) {
-                Toolkit.getDefaultToolkit().beep();
-            }
-        }
-    }
 }
 
