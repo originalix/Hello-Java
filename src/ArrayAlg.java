1ffa69cc8150740bf430cc5b1a11f8ffbb892a5f
Lix <xiao.liunit@gmail.com>
2017-07-30 15:34:34 +0800

    finish minMax function

--- src/ArrayAlg.java
+++ src/ArrayAlg.java
@@ -19,4 +19,21 @@ public class ArrayAlg {
             return second;
         }
     }
+
+    public static Pair minMax(double[] values) {
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+
+        for (double v : values) {
+            if (min > v) {
+                min = v;
+            }
+
+            if (max < v) {
+                max = v;
+            }
+        }
+
+        return new Pair(min, max);
+    }
 }
