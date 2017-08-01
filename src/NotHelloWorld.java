9a457550ad8487a3f6a61d9200385a2c684102ec
Lix <xiao.liunit@gmail.com>
2017-08-01 15:09:47 +0800

    finish not hello world programe

--- src/NotHelloWorld.java
+++ src/NotHelloWorld.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Lix on 2017/8/1.
  */
 public class NotHelloWorld {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new NotHelloWorldFrame();
+                frame.setTitle("Not Hello World Programe");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
 
 class NotHelloWorldFrame extends JFrame {
