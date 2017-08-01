4ff615689224fa31b137eb66da920291167eb8c8
Lix <xiao.liunit@gmail.com>
2017-08-01 14:29:49 +0800

    construction simple frame

--- src/SimpleFrame.java
+++ src/SimpleFrame.java
@@ -4,5 +4,10 @@ import javax.swing.*;
  * Created by Lix on 2017/8/1.
  */
 public class SimpleFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
 
+    public SimpleFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 }
