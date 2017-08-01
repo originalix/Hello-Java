0bb68f6fd748c62bca25982c53ec8fa7c68c6c04
Lix <xiao.liunit@gmail.com>
2017-08-01 13:52:52 +0800

    ovverride invoke function

--- src/TraceHandler.java
+++ src/TraceHandler.java
@@ -1,8 +1,31 @@
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 /**
  * Created by Lix on 2017/8/1.
  */
 public class TraceHandler implements InvocationHandler {
+    private Object target;
 
+    public TraceHandler(Object t) {
+        target = t;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.print(target);
+        System.out.print("." + method.getName() + "(");
+
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.print(")");
+
+        return method.invoke(target, args);
+    }
 }
