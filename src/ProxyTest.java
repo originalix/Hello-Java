7bb007116875dfd7f3909314706db3c5516f3ac0
Lix <xiao.liunit@gmail.com>
2017-08-01 14:23:51 +0800

    proxy test

---
+++ src/ProxyTest.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Lix on 2017/8/1.
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+        Object[] elements = new Object[1000];
+
+        for (int i = 0; i < elements.length; i++) {
+            Integer value = i + 1;
+            InvocationHandler handler = new TraceHandler(value);
+            Object proxy = Proxy.newProxyInstance(null, new Class[] { Comparable.class }, handler);
+            elements[i] = proxy;
+        }
+
+        Integer key = new Random().nextInt(elements.length) + 1;
+        int result = Arrays.binarySearch(elements, key);
+        if (result >= 0) {
+            System.out.println(elements[result]);
+        }
+    }
+}
