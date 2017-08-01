fc367b0922ca7ba52d289f0971a53711cd53210d
Lix <xiao.liunit@gmail.com>
2017-08-01 15:07:33 +0800

    finish not hello world frame class

--- src/NotHelloWorld.java
+++ src/NotHelloWorld.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 /**
  * Created by Lix on 2017/8/1.
  */
 public class NotHelloWorld {
 }
 
-
+class NotHelloWorldFrame extends JFrame {
+    public NotHelloWorldFrame() {
+        add(new NotHelloWorldComponent());
+        pack();
+    }
+}
