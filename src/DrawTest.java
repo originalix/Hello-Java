9ef32c2559670484fbe189b61c6b867b9f01e8ac
Lix <xiao.liunit@gmail.com>
2017-08-01 15:30:43 +0800

    finish draw component class

--- src/DrawTest.java
+++ src/DrawTest.java
@@ -1,5 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 /**
  * Created by Lix on 2017/8/1.
  */
 public class DrawTest {
+}
+
+class DrawComponent extends JComponent {
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        double leftX = 100;
+        double topY = 100;
+        double width = 200;
+        double height = 150;
+
+        Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
+        g2.draw(rect);
+
+        Ellipse2D ellipse = new Ellipse2D.Double();
+        ellipse.setFrame(rect);
+        g2.draw(ellipse);
+
+        g2.draw(new Line2D.Double(leftX, topY, leftX + width, topY + height));
+
+        double centerX = rect.getCenterX();
+        double centerY = rect.getCenterY();
+
+        double radius = 150;
+
+        Ellipse2D circle = new Ellipse2D.Double();
+        circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+        g2.draw(circle);
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 }
