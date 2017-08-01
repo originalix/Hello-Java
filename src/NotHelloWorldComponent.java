4fe7b47dc222ae5a3ca6593f6e458be27f9558de
Lix <xiao.liunit@gmail.com>
2017-08-01 15:05:15 +0800

    finish draw class

--- src/NotHelloWorldComponent.java
+++ src/NotHelloWorldComponent.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Lix on 2017/8/1.
  */
-public class NotHelloWorldComponent {
+public class NotHelloWorldComponent extends JComponent {
+    public static final int MESSAGE_X = 75;
+    public static final int MESSAGE_Y = 100;
+
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public void paintComponent(Graphics g) {
+        g.drawString("Not a Hello , world Program", MESSAGE_X, MESSAGE_Y);
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 }
