aef7525c69e7b0da9a37b14218a22c5385640ae0
Lix <xiao.liunit@gmail.com>
2017-08-01 14:50:12 +0800

    create sizedFrame class

--- src/SimpleFrameTest.java
+++ src/SimpleFrameTest.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Lix on 2017/8/1.
  */
 public class SimpleFrameTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                SimpleFrame frame = new SimpleFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
